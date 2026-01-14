de8b8bef7b7aa8569ab162567611f1158279677a
Matis Granval <208688787+matislebg95@users.noreply.github.com>
2026-01-14 18:07:16 +0100

    Adding description to products

--- app/controllers/store/products_controller.rb
+++ app/controllers/store/products_controller.rb
@@ -1,6 +1,7 @@
 class Store::ProductsController < Store::BaseController
   before_action :set_product, only: %i[ show edit update destroy ]
 
+
   def index
     @products = Product.all
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,6 @@ class Product < ApplicationRecord
   has_one_attached :featured_image
   validates :price_in_cents, presence: true
   validates :name, presence: true
+  validates :description, presence: true
   has_one_attached :description
 end
