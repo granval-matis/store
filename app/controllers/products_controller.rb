a955a1986790ff58dd45c1bd70eb728fdf4263ba
Matis Granval <208688787+matislebg95@users.noreply.github.com>
2026-03-11 21:28:53 +0100

    Add price filter in search interface

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,13 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
+    if params[:tata] == '1'
+      @search_results = Product.search("tata")
+    else
+      @search_results = Product.search(params[:search], { filter: "price_in_cents <= #{params[:max_price].to_f * 100}"})
+    end
+
+
   end
 
   def show
