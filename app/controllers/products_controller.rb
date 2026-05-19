4f3916916981c47a503ad7443d57e2e42f1b894c
Matis Granval <208688787+matislebg95@users.noreply.github.com>
2026-05-19 22:00:27 +0200

    Improve the style of the product page, reorganize the page and add a select color area

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,12 @@ class ProductsController < ApplicationController
     @products = Product.all
     if params[:tata] == '1'
       @search_results = Product.search("tata")
+    elsif (params[:search] == '') && (params[:max_price].to_i < 1)
+      @search_results = Product.all
+    elsif params[:search] == ''
+      @search_results = Product.search({ filter: "price_in_cents <= #{params[:max_price].to_f * 100}"})
+    elsif params[:max_price].to_i < 1
+      @search_results = Product.search(params[:search])
     else
       @search_results = Product.search(params[:search], { filter: "price_in_cents <= #{params[:max_price].to_f * 100}"})
     end
